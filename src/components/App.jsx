2a20d97430132cf788c312278ca5313f302f8184
KaerailearS <janttitaavi@gmail.com>
2025-04-22 14:40:00 +0300

    Created the new project

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import Main from './Main.jsx'
+  /**
+  * Challenge: Start a brand new React app!
+  * - Create a separate App component
+  * - Import and render the App component here
+  * - In the App component, render a <main> element
+  * - Style everything to look like the slide
+  */
+
+export default function App(){
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main(){
+  return (
+    <>
+      <div className="game-div">
+        <div className="tenzies-div">
+          <div className="active-div">
+            <h1 className="title">Tenzies</h1>
+            <p className="text">Roll until all dice are the same. Click each die to freeze it at its current value between rolls</p>
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}

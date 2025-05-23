2d716f4304570259de255a159c7e583af8ee2cb2
KaerailearS <janttitaavi@gmail.com>
2025-05-23 12:28:41 +0300

    Prettier formatting or something

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,14 +1,15 @@
-import styles from '../styles/Die.module.css'
+import styles from "../styles/Die.module.css";
 
 // rendering for individual Die component/buttons, receiving necessary data via props
-export default function Die({value, isHeld, onClick}){
+export default function Die({ value, isHeld, onClick }) {
   return (
     <button
       className={`${styles.die} ${isHeld ? styles.held : ""}`}
       onClick={onClick}
       aria-pressed={isHeld}
-      aria-label={`Die with value of ${value}, ${isHeld ? "held" : "not held"}`}>
-        {value}
-      </button>
+      aria-label={`Die with value of ${value}, ${isHeld ? "held" : "not held"}`}
+    >
+      {value}
+    </button>
   )
 }
